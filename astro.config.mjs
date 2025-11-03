095964eebaed23a2d3f1c70b232f5a8dbcadc0d6
stevenalp207 <s.alpizar207@gmail.com>
2025-11-03 08:48:24 -0600

    Add sitemap integration and update dependencies for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://agroticosmart.vercel.app', // 👈 obligatorio, URL base de tu web
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
